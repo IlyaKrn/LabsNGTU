65c0881a776788ac011daaa6ea366af6925d9d9c
IlyaKrn <ilyakornoukhov@gmail.com>
2025-03-07 21:17:05 +0300

    print + lab function

--- CCSMatrix.h
+++ CCSMatrix.h
@@ -21,7 +21,9 @@ public:
 
     void clear();
 
-    void print(void (*printer)(T item));
+    void printAsCCS(void (*printer)(T item));
+    void printAsMatrix(void (*printer)(T item), T zeroElement);
+    void getEvenIndexElements(void (*printer)(T item));
 };
 
 template<typename T>
@@ -36,15 +38,16 @@ CCSMatrix<T>::CCSMatrix(LinkedList<LinkedList<T>> matrix, int height, int width,
         }
     }
     for (int i = 0; i < width; ++i) {
-        LJ.pushBack(LI.getSize());
+        LJ.pushBack(LI.getSize() + 1);
         for (int j = 0; j < height; ++j) {
             T* item = matrix.getItemPtr(j)->getItemPtr(i);
             if(*item != zeroElement) {
                 values.pushBack(*item);
-                LI.pushBack(j);
+                LI.pushBack(j + 1);
             }
         }
     }
+    LJ.pushBack(LI.getSize() + 1);
 }
 
 template<typename T>
@@ -58,11 +61,63 @@ void CCSMatrix<T>::clear() {
 }
 
 template<typename T>
-void CCSMatrix<T>::print(void (*printer)(T)) {
+void CCSMatrix<T>::printAsCCS(void (*printer)(T)) {
     std::cout << "values: " << std::endl;
     values.print(printer);
+    auto printerInt = [](int i){
+        std::cout << i << " ";
+    };
     std::cout << "LI: " << std::endl;
-    LI.print(printer);
+    LI.print(printerInt);
     std::cout << "LJ: " << std::endl;
-    LJ.print(printer);
+    LJ.print(printerInt);
+}
+
+template<typename T>
+void CCSMatrix<T>::printAsMatrix(void (*printer)(T), T zeroElement) {
+    std::cout << "Matrix: " << std::endl;
+
+    int width = LJ.getSize() - 1;
+    int height = 0;
+    for (int i = 0; i < LI.getSize(); ++i) {
+        if(height < *LI.getItemPtr(i)){
+            height = *LI.getItemPtr(i);
+        }
+    }
+
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            bool isZero = true;
+            for (int k = *LJ.getItemPtr(j) - 1; k < *LJ.getItemPtr(j + 1) - 1; ++k) {
+                if(*LI.getItemPtr(k) == i + 1){
+                    isZero = false;
+                    printer(*values.getItemPtr(k));
+                }
+            }
+            if(isZero){
+                printer(zeroElement);
+            }
+
+        }
+        std::cout << std::endl;
+    }
+}
+
+template<typename T>
+void CCSMatrix<T>::getEvenIndexElements(void (*printer)(T)) {
+    std::cout << "Elements: " << std::endl;
+    for (int i = 0; i < values.getSize(); ++i) {
+        int y = *LI.getItemPtr(i);
+        int x;
+        for (int j = 0; j < LJ.getSize(); ++j) {
+            if(*LJ.getItemPtr(j) > i + 1)
+                break;
+                x = j + 1;
+        }
+        if((x + y) % 2 == 0) {
+            std::cout << x << ", " << y << ": ";
+            printer(*values.getItemPtr(i));
+            std::cout << std::endl;
+        }
+    }
 }
