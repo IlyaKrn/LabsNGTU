bde9171360b70df2a556931b60ad00a5b50734c1
IlyaKrn <ilyakornoukhov@gmail.com>
2025-03-15 01:35:11 +0300

    binary tree without print + printLogicFunction

--- BinaryTree.h
+++ BinaryTree.h
@@ -9,28 +9,64 @@ class BinaryTree {
 
 private:
     T _data;
-    BinaryTree* _right;
-    BinaryTree* _left;
+    BinaryTree *_right;
+    BinaryTree *_left;
 
 public:
     BinaryTree(T data);
-    BinaryTree(T data, BinaryTree* right, BinaryTree* left);
+
     ~BinaryTree();
-    void clear();
-    void setRight(BinaryTree* right);
-    void setLeft(BinaryTree* left);
-    void printBranchesWithMaxChilds(void (*printer)(T));
+
+    void setRightValue(T right);
+
+    void setLeftValue(T left);
+
+    BinaryTree *getRightPtr();
+
+    BinaryTree *getLeftPtr();
+//    void print(void (*printer)(T));
+//    void printBranchesWithMaxChilds(void (*printer)(T));
 
 };
 
 template<typename T>
-BinaryTree<T>::BinaryTree(T data, BinaryTree *right, BinaryTree *left) {
+BinaryTree<T>::BinaryTree(T data) {
     _data = data;
-    _right = right;
-    _left = left;
+    _left = nullptr;
+    _right = nullptr;
 }
 
 template<typename T>
-BinaryTree<T>::BinaryTree(T data) {
-    _data = data;
+BinaryTree<T>::~BinaryTree() {
+    delete _right;
+    delete _left;
 }
+
+template<typename T>
+void BinaryTree<T>::setLeftValue(T left) {
+    if (_left != nullptr)
+        delete _left;
+    _left = new BinaryTree(left);
+
+}
+
+template<typename T>
+void BinaryTree<T>::setRightValue(T right) {
+    if (_right != nullptr)
+        delete _right;
+    _right = new BinaryTree(right);
+
+}
+
+template<typename T>
+BinaryTree<T> *BinaryTree<T>::getLeftPtr() {
+    return _left;
+}
+
+template<typename T>
+BinaryTree<T> *BinaryTree<T>::getRightPtr() {
+    return _right;
+}
+
+
+
