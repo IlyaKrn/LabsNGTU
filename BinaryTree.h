efb02b6ab1650ea8db8d4e162ad7ae3957b567d5
IlyaKrn <ilyakornoukhov@gmail.com>
2025-03-19 14:01:56 +0300

    comments + file read

--- BinaryTree.h
+++ BinaryTree.h
@@ -6,6 +6,11 @@
 
 // Дано бинарное дерево. Найти ветви с мах числом ветвлений.
 
+/*
+ * структура branchingItem - вспомогательная структура для подсчета ветвлений
+ * _data - значение элемента для вывода
+ * isBranching - есть ли у узла оба дочерних узла
+ */
 template<typename T>
 struct branchingItem{
     T _data;
@@ -15,7 +20,19 @@ struct branchingItem{
     branchingItem();
 };
 
-
+/*
+ * BinaryTree - класс бинарного дерева
+ * _right - правый дочерний узел дерева
+ * _left- левый дочерний узел дерева
+ * getDeep - метод получения глубины дерева
+ * getBranches - метод получения всех ветвей
+ * setRightValue - метод добавления указателя правого узла
+ * setLeftValue - метод добавления указателя левого узла
+ * getRightPtr - метод получения указателя правого узла
+ * getLeftPtr - метод получения указателя левого узла
+ * print - метод вывода дерева в консоль
+ * printBranchesWithMaxChildren - метод вывода ветвей с максимальным числом ветвлений
+ */
 template<typename T>
 class BinaryTree {
 
@@ -49,17 +66,30 @@ public:
 
 };
 
+/*
+ * конструктор branchingItem
+ * data - значение элемента для вывода
+ * branching - есть ли у узла оба дочерних узла
+ */
 template<typename T>
 branchingItem<T>::branchingItem(T data, bool branching) {
     _data = data;
     isBranching = branching;
 }
 
+/*
+ * конструктор branchingItem
+ * конструктор не принимает параметры
+ */
 template<typename T>
 branchingItem<T>::branchingItem() {
     isBranching = false;
 }
 
+/*
+ * конструктор BinaryTree
+ * data - значение элемента для узла
+ */
 template<typename T>
 BinaryTree<T>::BinaryTree(T data) {
     _data = data;
@@ -67,8 +97,12 @@ BinaryTree<T>::BinaryTree(T data) {
     _right = nullptr;
 }
 
+/*
+ * деструктор BinaryTree
+ */
 template<typename T>
 BinaryTree<T>::~BinaryTree() {
+    //проверка на уже освобожденную память и освобождение
     try{
         _left = nullptr;
         delete _left;
@@ -79,71 +113,119 @@ BinaryTree<T>::~BinaryTree() {
     }catch(...) {}
 }
 
+/*
+ * Метод setLeftValue
+ * left - значение левого узла
+ * метод ничего не возвращает
+ */
 template<typename T>
 void BinaryTree<T>::setLeftValue(T left) {
+    //освобождаем память если узел не пуст
     if (_left != nullptr)
         delete _left;
     _left = new BinaryTree(left);
 
 }
 
+/*
+ * Метод setLeftValue
+ * right - значение правого узла
+ * метод ничего не возвращает
+ */
+
 template<typename T>
 void BinaryTree<T>::setRightValue(T right) {
+    //освобождаем память если узел не пуст
     if (_right != nullptr)
         delete _right;
     _right = new BinaryTree(right);
 
 }
 
+/*
+ * Метод getLeftPtr
+ * метод ничего не принимает
+ * метод возвращает указатель левого узла
+ */
+
 template<typename T>
 BinaryTree<T> *BinaryTree<T>::getLeftPtr() {
     return _left;
 }
+
+/*
+ * Метод getRightPtr
+ * метод ничего не принимает
+ * метод возвращает указатель правого узла
+ */
 
 template<typename T>
 BinaryTree<T> *BinaryTree<T>::getRightPtr() {
     return _right;
 }
 
+/*
+ * Метод getDeep
+ * curDeep - текущая глубина рекурсии
+ * метод возвращает максимальную длину ветви
+ */
+
 template<typename T>
 int BinaryTree<T>::getDeep(int curDeep) {
+    //выход из рекурсии на листе
     if(_left == nullptr && _right == nullptr){
         return curDeep + 1;
     }
 
+    //возвращаем самое большое значение функции от дочерних узлов
     int leftDeep = 0;
     int rightDeep = 0;
     if(_left != nullptr){
         leftDeep = _left->getDeep(curDeep + 1);
     }
-    if(_left != nullptr){
+    if(_right != nullptr){
         rightDeep = _right->getDeep(curDeep + 1);
     }
 
     return (leftDeep > rightDeep ? leftDeep : rightDeep);
 }
 
+/*
+ * Метод print
+ * printer - функция вывода типа T в консоль
+ * метод ничего не возвращает
+ */
 template<typename T>
 void BinaryTree<T>::print(void (*printer)(T, bool)) {
+    // добавляем в очередь корень
     LinkedList<BinaryTree<T>> queue = LinkedList<BinaryTree<T>>();
 
     queue.pushBack(*this, false);
+    //костыль для работы со списком как с очередью (push = pushBack, pop = queueIndex++)
     int queueIndex = 0;
     int levelCount = 0;
+    //глубина дерева
     int deep = getDeep(0);
+    //число символов гна элемент
     int cellsForElement = pow(2, deep);
+    //проходим по очереди не дойдем до последнего уровня
     while (levelCount < deep) {
+        //количество элементов в слое
         int levelSize = queue.getSize() - queueIndex;
         levelCount ++;
+        //проходим по слою
         for (int i = 0; i < levelSize; ++i) {
+            //выводим текущий элемент либо пробел, если он пустой
             BinaryTree* item = queue.getItemPtr(queueIndex);
             queueIndex++;
             printer(item->_data, queue.isEmpty(queueIndex - 1));
 
+            //вывод пробелов для красивого отображения слоя
             for (int j = 0; j < cellsForElement - 1; ++j) {
                 std::cout << " ";
             }
 
+            //добавляем дочерние узлы или пустые узлы в очередь, если текущий узел пустой
             if (item->_left != nullptr) {
                 queue.pushBack(*(item->_left), false);
             }
@@ -157,54 +239,78 @@ void BinaryTree<T>::print(void (*printer)(T, bool)) {
                 queue.pushBack(_data, true);
             }
         }
+        //уменьшаем количество символов для элементов следующего слоя
         cellsForElement /= 2;
         std::cout << std::endl;
     }
 }
 
+/*
+ * Метод getBranches
+ * currentStack - текущая ветвь
+ * allBranches - список всех ветвей
+ * метод ничего не возвращает
+ */
 template<typename T>
 void BinaryTree<T>::getBranches(LinkedList<BinaryTree> currentStack, LinkedList<LinkedList<branchingItem<T>>> *allBranches) {
+    //текущий элемент ветви
     BinaryTree<T> cur = *(currentStack.getItemPtr(currentStack.getSize() - 1));
+    //выход из рекурсии если текущий - лист
     if(cur._left == nullptr && cur._right == nullptr){
+        //формирование новой ветки и добавление в список
         LinkedList<branchingItem<T>> newBranch = LinkedList<branchingItem<T>>();
         for (int i = 0; i < currentStack.getSize(); ++i) {
             BinaryTree<T> currentItem = *(currentStack.getItemPtr(i));
+            //элемент ветви с параметром ветвления
             branchingItem<T> it = branchingItem<T>(currentItem._data, (currentItem._left != nullptr && currentItem._right != nullptr));
             newBranch.pushBack(it, false);
         }
         allBranches->pushBack(newBranch, false);
         return;
     }
+    //добавление дочерних узлов и продолжение рекурсии
     if(cur._left != nullptr){
+        //копирование ветви + следующий элемент
         LinkedList<BinaryTree> nextStack = LinkedList<BinaryTree>();
         for (int i = 0; i < currentStack.getSize(); ++i) {
             nextStack.pushBack(*(currentStack.getItemPtr(i)), false);
         }
         nextStack.pushBack(*cur._left, false);
+        //заполнение списка ветвей
         getBranches(nextStack, allBranches);
     }
     if(cur._right != nullptr){
+        //копирование ветви + следующий элемент
         LinkedList<BinaryTree> nextStack = LinkedList<BinaryTree>();
         for (int i = 0; i < currentStack.getSize(); ++i) {
             nextStack.pushBack(*(currentStack.getItemPtr(i)), false);
         }
         nextStack.pushBack(*cur._right, false);
+        //заполнение списка ветвей
         getBranches(nextStack, allBranches);
     }
 }
 
+/*
+ * Метод printBranchesWithMaxChildren
+ * printer - функция вывода типа T в консоль
+ * метод ничего не возвращает
+ */
 template<typename T>
 void BinaryTree<T>::printBranchesWithMaxChildren(void (*printer)(T, bool)) {
 
+    //получение списка ветвей
     LinkedList<LinkedList<branchingItem<T>>> all = LinkedList<LinkedList<branchingItem<T>>>();
     LinkedList<BinaryTree> current = LinkedList<BinaryTree>();
 
     current.pushBack(*this, false);
 
     getBranches(current, &all);
+    //получение максимального количества ветвлений
     int maxCount = 0;
     for (int i = 0; i < all.getSize(); ++i) {
         int branching = 0;
+        //не учитываем корень, лист, предпоследний элемент
         for (int j = 1; j < all.getItemPtr(i)->getSize() - 2; ++j) {
             if(all.getItemPtr(i)->getItemPtr(j)->isBranching){
                 branching++;
@@ -214,13 +320,16 @@ void BinaryTree<T>::printBranchesWithMaxChildren(void (*printer)(T, bool)) {
             maxCount = branching;
         }
     }
+    //вывод нужных ветвей
     for (int i = 0; i < all.getSize(); ++i) {
+        //считаем ветвления
         int branching = 0;
         for (int j = 1; j < all.getItemPtr(i)->getSize() - 2; ++j) {
             if(all.getItemPtr(i)->getItemPtr(j)->isBranching){
                 branching++;
             }
         }
+        //если это максимальное число ветвлений, выводим ветвь
         if(branching == maxCount){
             std::cout << branching << ": ";
             for (int j = 0; j < all.getItemPtr(i)->getSize(); ++j) {
@@ -235,6 +344,5 @@ void BinaryTree<T>::printBranchesWithMaxChildren(void (*printer)(T, bool)) {
 
 
 //ввод дерева с консоли
-//комментарии
 
 
--- main.cpp
+++ main.cpp
@@ -6,14 +6,143 @@
 
 using namespace std;
 
-int getTreeFromFile(BinaryTree<int> *treePtr, string &filePath) {
-    return 0;
+void getTreeFromFile(BinaryTree<int> *tree) {
+    bool repeat = false;
+    do{
+        // открытие файла
+        string filepath;
+        cout << "Enter path to file:" << endl;
+        cin >> filepath;
+//        filepath = "../input.txt";
+        ifstream file(filepath);
+
+        //если файл открыт
+        if (file.is_open()){
+            string input;
+            int deep;
+
+            //проверка файла
+            if(file.eof()) {
+                repeat = true;
+                cout << "can not parse file" << endl;
+            }
+            try{
+                //получение глубины и корня дерева
+                getline(file, input);
+                deep = stoi(input);
+                getline(file, input);
+                int rootVal = stoi(input);
+                *(tree) = BinaryTree<int>(rootVal);
+
+                //очередь для текущего слоя, в который записывать дочерние элементы (добавляем корень)
+                LinkedList<BinaryTree<int>*> queue = LinkedList<BinaryTree<int>*>();
+                int queueIndex = 0;
+                queue.pushBack(tree, false);
+
+                //читаем строку из файла и записываем ее элементы в дерево
+                for (int i = 1; i < deep; ++i) {
+                    //проверка файла
+                    if(file.eof()) {
+                        repeat = true;
+                        cout << "can not parse file" << endl;
+                    }
+                    //чтение строки
+                    getline(file, input);
+                    LinkedList<int> row;
+                    string item;
+                    for (int j = 0; j < input.size(); ++j) {
+                        if(input.at(j) != ' '){
+                            item += input.at(j);
+                        }
+                        else{
+                            //записываем элемент и при необходимости помечаем его пустым
+                            if(item == "-") {
+                                row.pushBack(-1, true);
+                            }
+                            else{
+                                row.pushBack(stoi(item), false);
+                            }
+                            item.clear();
+                        }
+                    }
+                    //вывод прочитанных списков
+//                    row.print([](int d, bool b){
+//                        if(b){
+//                            cout << "-";
+//                        }
+//                        else{
+//                            cout << d;
+//                        }
+//                    });
+//                    cout << endl;
+
+                    //добавление в дерево
+                    for (int j = 0; j < row.getSize(); j += 2) {
+                        //текущее значение
+                        int curLeft = *(row.getItemPtr(j));
+                        int curRight = *(row.getItemPtr(j + 1));
+                        bool isLeft = !row.isEmpty(j);
+                        bool isRight = !row.isEmpty(j + 1);
+
+                        //текущий узел
+                        BinaryTree<int> *curRoot = *(queue.getItemPtr(queueIndex));
+                        queueIndex++;
+
+                        //добавляем дочерние узлы в дерево и очередь, если пустые,
+                        //то в очереди помечием пустыми
+                        if(isLeft){
+                            curRoot->setLeftValue(curLeft);
+                            queue.pushBack(curRoot->getLeftPtr(), false);
+                        }
+                        else{
+                            queue.pushBack(nullptr, true);
+                        }
+                        if(isRight){
+                            curRoot->setRightValue(curRight);
+                            queue.pushBack(curRoot->getRightPtr(), false);
+                        }
+                        else{
+                            queue.pushBack(nullptr, true);
+                        }
+                    }
+                }
+            } catch (...){
+                //ошибка чтения данных
+                repeat = true;
+                cout << "can not parse file" << endl;
+                continue;
+            }
+            //закрываем файл и возвращаем матрицу CCS
+            file.close();
+            repeat = false;
+        }
+        else{
+            //ошибка чтения файла
+            repeat = true;
+            cout << "can not read file" << endl;
+        }
+    } while (repeat);
+
 }
 
 int main() {
-    auto printer = [](int item) {
-        cout << item;
+    //функция вывода элемента в консоль
+    auto printer = [](int item, bool isEmpty) {
+        cout << (isEmpty ? "-" : to_string(item));
     };
+
+    //получение дерева
+    BinaryTree<int> root = BinaryTree<int>(0);
+    getTreeFromFile(&root);
+
+
+    //вывод результата в консоль
+    cout << "=================================" << endl;
+    root.print(printer);
+    cout << "=================================" << endl;
+    root.printBranchesWithMaxChildren(printer);
+    cout << "=================================" << endl;
+
 
     return 0;
 }
