8e3c740c73be567d33ab193b6b20750547dfa1b6
IlyaKrn <ilyakornoukhov@gmail.com>
2025-04-27 20:55:22 +0300

    white bix test

--- test.cpp
+++ test.cpp
@@ -59,10 +59,49 @@ int stressTest(){
     return elapsed_seconds.count();
 }
 
+int whiteBox() {
+    LinkedList<LinkedList<int>> matrixTemplate = LinkedList<LinkedList<int>>();
+    static int evenCount = 0;
+    for (int i = 0; i < 10; ++i) {
+        LinkedList<int> row = LinkedList<int>();
+        for (int j = 0; j < 10; ++j) {
+            if((i + j) % 2 == 0) {
+                row.pushBack(2);
+                evenCount++;
+            } else{
+                row.pushBack(0);
+            }
+        }
+        matrixTemplate.pushBack(row);
+    }
+
+    try {
+        CCSMatrix<int> matrix = CCSMatrix<int>(matrixTemplate, 1, 1, 0);
+        return 1;
+    }catch (...){
+
+    }
+
+    try {
+        CCSMatrix<int> matrix = CCSMatrix<int>(matrixTemplate, 100, 100, 0);
+        return 1;
+    }catch (...){
+
+    }
+
+    try {
+        CCSMatrix<int> matrix = CCSMatrix<int>(matrixTemplate, 10, 10, 0);
+    }catch (...){
+        return 1;
+    }
+    return 0;
+}
+
 
 int main(){
     cout << "unit test " << (unitTest() == 0 ? "passed" : "failed") << endl;
     cout << "unit test passed in " << stressTest() << " seconds" << endl;
+    cout << "unit test " << (whiteBox() == 0 ? "passed" : "failed") << endl;
 
     return 0;
 }
