3b0eda75012595404b90ae1c5ca84939697ee295
IlyaKrn <ilyakornoukhov@gmail.com>
2025-05-19 21:32:55 +0300

    drawing field

--- main.cpp
+++ main.cpp
@@ -1,140 +1,76 @@
 #include "include/FieldMatrix.h"
 #include "include/GameLogic.h"
 #include <iostream>
-#include <stdlib.h>
+#include "SDL.h"
 
-// Вспомогательная функция для печати поля
-void printField(FieldMatrix* field, int sizeX, int sizeY) {
-    for (int y = 0; y < sizeY; ++y) {
-        for (int x = 0; x < sizeX; ++x) {
-            std::cout << "\x1b[31m";
-            if ((x) % 2 == 0)
-                std::cout << "\x1b[34m";
-            cell* c = field->getPtr(x, y);
-            if (c != nullptr && c->isLine) {
-                if (c->state == BLACK) {
-                    std::cout << "○ ";
-                } else if (c->state == WHITE) {
-                    std::cout << "● ";
-                } else {
-                    switch (c->lineIn) {
-                        case RIGHT:
-                            switch (c->lineOut) {
-                                case LEFT:
-                                    std::cout << "--";
-                                    break;
-                                case TOP:
-                                    std::cout << "\\-";
-                                    break;
-                                case BOTTOM:
-                                    std::cout << "/-";
-                                    break;
-                                default:
-                                    std::cout << "  ";
-                            }
-                            break;
-                        case LEFT:
-                            switch (c->lineOut) {
-                                case RIGHT:
-                                    std::cout << "--";
-                                    break;
-                                case TOP:
-                                    std::cout << "-/";
-                                    break;
-                                case BOTTOM:
-                                    std::cout << "-\\";
-                                    break;
-                                default:
-                                    std::cout << "  ";
-                            }
-                            break;
-                        case TOP:
-                            switch (c->lineOut) {
-                                case RIGHT:
-                                    std::cout << "\\-";
-                                    break;
-                                case LEFT:
-                                    std::cout << "-/";
-                                    break;
-                                case BOTTOM:
-                                    std::cout << "| ";
-                                    break;
-                                default:
-                                    std::cout << "  ";
-                            }
-                            break;
-                        case BOTTOM:
-                            switch (c->lineOut) {
-                                case RIGHT:
-                                    std::cout << "/-";
-                                    break;
-                                case LEFT:
-                                    std::cout << "-\\";
-                                    break;
-                                case TOP:
-                                    std::cout << "| ";
-                                    break;
-                                default:
-                                    std::cout << "  ";
-                            }
-                            break;
-                        default:
-                            std::cout << "  ";
+using namespace std;
+
+void printField(FieldMatrix* fieldMatrix, SDL_Window* window, int height, int width, int pixelSize){
+    unsigned char* pixels = (unsigned char*)SDL_GetWindowSurface(window)->pixels;
+    for (int startX = 0; startX < width * pixelSize; startX += pixelSize) {
+        for (int startY = 0; startY < height * pixelSize; startY += pixelSize) {
+            for (int pxX = 0; pxX < pixelSize; pxX++) {
+                for (int pxY = 0; pxY < pixelSize; pxY++) {
+
+                    bool isFill = false;
+
+                    if (pxX < 4 || pxY < 4) {
+                        isFill = true;
                     }
+                    else if (fieldMatrix->getPtr(startX / pixelSize, startY / pixelSize) != nullptr) {
+                        cell* curCell = fieldMatrix->getPtr(startX / pixelSize, startY / pixelSize);
+                        switch (curCell->state) {
+                            case WHITE:
+                                if ((pxX >= 10 && pxX < pixelSize - 6) && (pxY >= 10 && pxY < pixelSize - 6))
+                                    isFill = true;
+                                if((pxX >= 16 && pxX < pixelSize - 12) && (pxY >= 16 && pxY < pixelSize - 12))
+                                    isFill = false;
+                                break;
+                            case BLACK:
+                                if ((pxX >= 10 && pxX < pixelSize - 6) && (pxY >= 10 && pxY < pixelSize - 6))
+                                    isFill = true;
+                                break;
+                            default:
+                                isFill = false;
+                        }
+                        if (curCell->isLine){
+
+                        }
+                    }
+
+                    pixels[4 * ((startY + pxY) * width * pixelSize + (startX + pxX)) + 0] = isFill ? 0 : 255;
+                    pixels[4 * ((startY + pxY) * width * pixelSize + (startX + pxX)) + 1] = isFill ? 0 : 255;
+                    pixels[4 * ((startY + pxY) * width * pixelSize + (startX + pxX)) + 2] = isFill ? 0 : 255;
+                    pixels[4 * ((startY + pxY) * width * pixelSize + (startX + pxX)) + 3] = 255;
+
                 }
-            } else {
-                std::cout << "  ";
             }
         }
-        std::cout << std::endl;
     }
+    SDL_UpdateWindowSurface(window);
+
 }
 
 int main() {
+
+    int height = 6;
+    int width = 6;
+    int pxSize = 50;
+
+    SDL_Window* window = SDL_CreateWindow("Ожерелье", 0, 0, height * pxSize, width * pxSize, 0);
+
     FieldMatrix* field = new FieldMatrix();
-
-//    field->insert(cell(WHITE, RIGHT, RIGHT, false), 2, 0);
-//    field->insert(cell(BLACK, RIGHT, RIGHT, false), 5, 0);
-//
-//    field->insert(cell(BLACK, RIGHT, RIGHT, false), 0, 2);
-//    field->insert(cell(BLACK, RIGHT, RIGHT, false), 3, 2);
-//    field->insert(cell(WHITE, RIGHT, RIGHT, false), 4, 2);
-//    field->insert(cell(BLACK, RIGHT, RIGHT, false), 7, 2);
-//    field->insert(cell(WHITE, RIGHT, RIGHT, false), 9, 2);
-//
-//    field->insert(cell(WHITE, RIGHT, RIGHT, false), 6, 3);
-//
-//    field->insert(cell(BLACK, RIGHT, RIGHT, false), 2, 4);
-//    field->insert(cell(BLACK, RIGHT, RIGHT, false), 7, 4);
-//    field->insert(cell(WHITE, RIGHT, RIGHT, false), 8, 4);
-//    field->insert(cell(WHITE, RIGHT, RIGHT, false), 9, 4);
-//
-//    field->insert(cell(WHITE, RIGHT, RIGHT, false), 5, 5);
-//
-//    field->insert(cell(WHITE, RIGHT, RIGHT, false), 1, 6);
-//    field->insert(cell(WHITE, RIGHT, RIGHT, false), 3, 6);
-//    field->insert(cell(WHITE, RIGHT, RIGHT, false), 6, 6);
-//    field->insert(cell(WHITE, RIGHT, RIGHT, false), 7, 6);
-//
-//    field->insert(cell(WHITE, RIGHT, RIGHT, false), 1, 7);
-//    field->insert(cell(WHITE, RIGHT, RIGHT, false), 4, 7);
-//
-//    field->insert(cell(BLACK, RIGHT, RIGHT, false), 8, 8);
-//
-//    field->insert(cell(BLACK, RIGHT, RIGHT, false), 0, 9);
-//    field->insert(cell(WHITE, RIGHT, RIGHT, false), 2, 9);
-//    field->insert(cell(WHITE, RIGHT, RIGHT, false), 5, 9);
-
-
-
     field->insert(cell(BLACK, RIGHT, RIGHT, false), 0, 0);
     field->insert(cell(WHITE, RIGHT, RIGHT, false), 2, 2);
     field->insert(cell(BLACK, RIGHT, RIGHT, false), 4, 4);
 
-    GameLogic gl(field, 5, 5);
-    gl.getSolution();
+    printField(field, window, height, width, pxSize);
 
-    printField(field, 5, 5);
+//    GameLogic gl(field, 5, 5);
+//    gl.getSolution();
+
+    while (true){}
+
     delete field;
     return 0;
 }
