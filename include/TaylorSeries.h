752803aa4bc975ca248e5825d1c7165d3eb167f7
IlyaKrn <ilyakornoukhov@gmail.com>
2025-09-11 23:07:59 +0300

    сделал тв кроме вычисления границ погрешности

--- include/TaylorSeries.h
+++ include/TaylorSeries.h
@@ -1,3 +1,4 @@
 #pragma once
 
 float taylorSerial(float x, float x0, float k, int members);
+//float taylorSerialErrorVal(float x, float x0, float k, int members);
--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@ using namespace std;
 int main(int argc, char** argv) {
 
     //размеры окна и размер клетки
-    int HEIGHT = 1000;
-    int WIDTH = 1000;
-    int SCALE = 50;
+    int HEIGHT = 300;
+    int WIDTH = 300;
+    int SCALE = 15;
 
 
     //открываем окно и получаем матрицу пикселей
@@ -18,37 +18,82 @@ int main(int argc, char** argv) {
     Uint32* pixels = (Uint32*) SDL_GetWindowSurface(window)->pixels;
 
 
-    //отрисовываем изображение и считываем действия пользователя
+    //регулируемые переменные
+    int members = 1;
+    float k = 1;
+    float x0 = 0;
+    float EPS = 0.1;
+
+    //рисуем изображение и считываем действия пользователя
     while (true){
         //читаем события
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
-                case SDL_MOUSEBUTTONDOWN:
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.sym) {
+                        case SDLK_UP:
+                            k += 0.05;
+                            break;
+                        case SDLK_DOWN:
+                            k -= 0.05;
+                            break;
+                        case SDLK_RIGHT:
+                            x0 += 0.1;
+                            break;
+                        case SDLK_LEFT:
+                            x0 -= 0.1;
+                            break;
+                        case SDLK_0: members = 1;
+                            break;
+                        case SDLK_1: members = 2;
+                            break;
+                        case SDLK_2: members = 3;
+                            break;
+                        case SDLK_3: members = 4;
+                            break;
+                        case SDLK_4: members = 5;
+                            break;
+                        case SDLK_5: members = 6;
+                            break;
+                        case SDLK_6: members = 7;
+                            break;
+                        case SDLK_7: members = 8;
+                            break;
+                        case SDLK_8: members = 9;
+                            break;
+                        case SDLK_9: members = 10;
+                            break;
+                    }
                     break;
                 case SDL_QUIT:
                     SDL_Quit();
                     return 0;
             }
+            cout << "members: " << members << endl;
+            cout << "k: " << k << endl;
+            cout << "x0: " << x0 << endl;
+            cout << endl;
         }
 
         //отрисовка функций
         for (int y = 0; y < HEIGHT; ++y) {
             for (int x = 0; x < WIDTH; ++x) {
                 //считаем координаты с поправкой на смещение осей и переворот
-                int fixX = x - WIDTH/2;
-                int fixY = - y + HEIGHT/2;
-                //вычисляем значения функций для текущего x с поправкой на перевернутую ось OY и смещение осей
-                float funcVal = sin((float) fixX / SCALE) * SCALE;
-                float taylorVal = taylorSerial((float) fixX / SCALE, 0, 1, 10) * SCALE;
+                float fixX = x - WIDTH/2;
+                float fixY = - y + HEIGHT/2;
+                //вычисляем значения функций для текущего x
+                float funcVal = sin(k * fixX / (float) SCALE) * SCALE;
+                float taylorVal = taylorSerial(fixX / (float) SCALE, x0, k, members) * SCALE;
 
                 //красим пиксель в нужный цвет
                 pixels[y * WIDTH + x] =
                         fixX == 0 ||
                         fixY == 0 ||
+                        (fixX + 2 > x0 * SCALE && fixX - 2 < x0 * SCALE) ||
                         (fixY + 2 > funcVal && fixY - 2 < funcVal) ||
                         (fixY + 2 > taylorVal && fixY - 2 < taylorVal)
-                        ? 0 : 2147483647;
+                        ? (abs(funcVal - taylorVal) < EPS * SCALE ? 555555 : 0) : 2147483647;
 
             }
         }
