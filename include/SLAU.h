29066e12be557f57ba70dc7def5475253221efb0
IlyaKrn <ilyakornoukhov@gmail.com>
2025-12-01 20:00:29 +0300

    слау

---
+++ include/SLAU.h
@@ -0,0 +1,7 @@
+#include "vector"
+
+
+void printSLAU(std::vector<std::vector<long double>>);
+std::vector<std::vector<std::vector<long double>>> getSLAUs();
+std::vector<std::vector<long double>> sortSLAU(std::vector<std::vector<long double>>);
+std::vector<std::vector<long double>> solveSLAU(std::vector<std::vector<long double>>);
---
+++ src/SLAU.cpp
@@ -0,0 +1,19 @@
+#include "../include/SLAU.h"
+
+using namespace std;
+
+std::vector<std::vector<std::vector<long double>>> getSLAUs(){
+
+}
+
+void printSLAU(vector<vector<long double>>){
+    
+}
+
+vector<vector<long double>> sortSLAU(vector<vector<long double>>){
+
+}
+
+vector<vector<long double>> solveSLAU(vector<vector<long double>>){
+
+}
