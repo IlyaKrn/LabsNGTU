dfd42152da66fd3612cba8a38d72362fd1b56af2
IlyaKrn <ilyakornoukhov@gmail.com>
2025-12-07 15:24:43 +0300

    начал фортран

--- fortran/main.f90
+++ fortran/main.f90
@@ -1,159 +1,111 @@
-module m_module
+program slau_solver
     implicit none
+
+    integer :: n, i, j, row, max_row_index, num_systems
+    real(8), allocatable :: a(:,:), answer(:)
+    real(8) :: max_el, mnozh, sum
+    character(len=256) :: filename
+    integer :: io
+
+    filename = "/home/ilyakrn/CLionProjects/LabsNGTU/nodes.txt"
+
+    open(unit=10, file=filename, status='old', action='read')
+
+    do
+        read(10, *, iostat=io) n
+        if (io /= 0) exit  ! конец файла
+
+        allocate(a(n, n+1))
+        allocate(answer(n))
+
+        ! читаем матрицу
+        do i = 1, n
+            do j = 1, n+1
+                read(10, *) a(i, j)
+            end do
+        end do
+
+        print *, "============================"
+        call print_slau(n, a)
+
+        ! --- Гауссово исключение ---
+        do row = 2, n
+            max_el = 0.0d0
+            max_row_index = row - 1
+
+            ! поиск максимального элемента
+            do i = row - 1, n
+                if (abs(a(i, row - 1)) > abs(max_el)) then
+                    max_el = a(i, row - 1)
+                    max_row_index = i
+                end if
+            end do
+
+            ! обмен строк
+            if (max_row_index /= row - 1) then
+                call swap_rows(a, row - 1, max_row_index, n)
+            end if
+
+            ! обнуление столбца ниже диагонали
+            do i = row, n
+                mnozh = a(i, row - 1) / a(row - 1, row - 1)
+                do j = row - 1, n+1
+                    a(i, j) = a(i, j) - mnozh * a(row - 1, j)
+                end do
+                a(i, row - 1) = 0.0d0
+            end do
+        end do
+
+        print *, ""
+        call print_slau(n, a)
+
+        ! --- обратный ход ---
+        do i = n, 1, -1
+            sum = 0.0d0
+            do j = 1, n
+                sum = sum + answer(j) * a(i, j)
+            end do
+            answer(i) = (a(i, n+1) - sum) / a(i, i)
+        end do
+
+        print *, ""
+        do i = 1, n
+            write(*,'(F12.6)', advance='no') answer(i)
+        end do
+        print *, ""
+        print *, "============================"
+
+        deallocate(a)
+        deallocate(answer)
+    end do
+
+    close(10)
+
 contains
 
-    ! процедура для чтения узлов аппроксимации
-    subroutine get_nodes(x, y, n)
-        implicit none
-        real(8), allocatable, intent(inout) :: x(:)
-        real(8), allocatable, intent(inout) :: y(:)
-        integer, intent(inout) :: n
-
-        ! индекс текущего узла
-        integer :: node
-        ! открывайм файл, читаем кол-во узлов и выделяем память
-        open(unit=0,file='nodes.txt',status='old',action='read')
-        read(0, *) n
-        allocate(x(n))
-        allocate(y(n))
-        ! читаем узлы
-        do node = 1, n
-            read(0, *) x(node)
-            read(0, *) y(node)
-        end do
-        ! закрываем файл
-        close(0)
-    end subroutine get_nodes
-
-    ! процедура для вычисления коэффициентов полинома
-    subroutine get_coeffs(x, y, n, coeffs)
-        implicit none
-        real(8), allocatable, intent(inout) :: x(:)
-        real(8), allocatable, intent(inout) :: y(:)
-        integer, intent(inout) :: n
-        real(8), intent(inout) :: coeffs(4)
-
-        ! слау для вычисления коэффициентов
+    subroutine print_slau(n, a)
+        integer, intent(in) :: n
+        real(8), intent(in) :: a(n, n+1)
         integer :: i, j
-        real(8) :: slau(4, 5)
-        real(8) :: xDegrees(7)
-        real(8) :: right(4)
-        real(8) :: mnozh
-
-        ! вычисляем необходимые суммы
         do i = 1, n
-            xDegrees(1) = xDegrees(1) + 1
-            xDegrees(2) = xDegrees(2) + x(i)
-            xDegrees(3) = xDegrees(3) + x(i) * x(i)
-            xDegrees(4) = xDegrees(4) + x(i) * x(i) * x(i)
-            xDegrees(5) = xDegrees(5) + x(i) * x(i) * x(i) * x(i)
-            xDegrees(6) = xDegrees(6) + x(i) * x(i) * x(i) * x(i) * x(i)
-            xDegrees(7) = xDegrees(7) + x(i) * x(i) * x(i) * x(i) * x(i) * x(i)
-
-            right(1) = right(1) + y(i)
-            right(2) = right(2) + y(i) * x(i)
-            right(3) = right(3) + y(i) * x(i) * x(i)
-            right(4) = right(4) + y(i) * x(i) * x(i) * x(i)
-        end do
-
-        ! составляем слау
-        do i = 1, 4
-            do j = 1, 4
-                slau(i, j) = xDegrees(i + j - 1)
+            do j = 1, n
+                write(*,'(F12.6)', advance='no') a(i,j)
+                write(*,'(A)', advance='no') " "
             end do
-            slau(i, 5) = right(i)
+            write(*,'(A,F12.6)') "= ", a(i, n+1)
         end do
+    end subroutine print_slau
 
-        ! зануляем столбцы
-        do i = 2, 4
-            mnozh = slau(i, 1) / slau(1, 1)
-            do j = 1, 5
-                slau(i, j) = slau(i, j) - mnozh * slau(1, j)
-            end do
+    subroutine swap_rows(a, r1, r2, n)
+        real(8), intent(inout) :: a(:,:)
+        integer, intent(in) :: r1, r2, n
+        real(8) :: tmp
+        integer :: j
+        do j = 1, n+1
+            tmp = a(r1, j)
+            a(r1, j) = a(r2, j)
+            a(r2, j) = tmp
         end do
-        do i = 3, 4
-            mnozh = slau(i, 2) / slau(2, 2)
-            do j = 2, 5
-                slau(i, j) = slau(i, j) - mnozh * slau(2, j)
-            end do
-        end do
-        do i = 4, 4
-            mnozh = slau(i, 3) / slau(3, 3)
-            do j = 3, 5
-                slau(i, j) = slau(i, j) - mnozh * slau(3, j)
-            end do
-        end do
+    end subroutine swap_rows
 
-        ! считаем ответ
-        coeffs(4) = (slau(4, 5)) / slau(4, 4)
-        coeffs(3) = (slau(3, 5) - coeffs(4) * slau(3, 4)) / slau(3, 3)
-        coeffs(2) = (slau(2, 5) - coeffs(4) * slau(2, 4) - coeffs(3) * slau(2, 3)) / slau(2, 2)
-        coeffs(1) = (slau(1, 5) - coeffs(4) * slau(1, 4) - coeffs(3) * slau(1, 3) - coeffs(2) * slau(1, 2)) / slau(1, 1)
-
-    end subroutine get_coeffs
-
-end module m_module
-
-
-program main
-    use m_module
-    use plplot
-    implicit none
-
-    ! узлы аппроксимации и коэфициенты
-    integer :: n
-    real(8), allocatable :: x(:)
-    real(8), allocatable :: y(:)
-    real(8) :: coeffs(4)
-
-    ! отрисовка графиков
-    integer :: i, j, points_count
-    real(8) :: xmin, xmax, ymin, ymax
-    real(8), allocatable :: x_func(:), y_func(:)
-
-    ! получаем узлы и коэффициенты
-    call get_nodes(x, y, n)
-    call get_coeffs(x, y, n, coeffs)
-
-    ! настраиваем параметры отрисовки
-    points_count = 1000
-    xmin = -1
-    xmax = 101
-    ymin = 0
-    ymax = 0
-    do i= 1, n
-        if (ymin > y(i)) then
-            ymin = y(i)
-        endif
-        if (ymax < y(i)) then
-            ymax = y(i)
-        endif
-    end do
-    ymin = 0.99
-    ymax = 1.01
-
-    ! вычисляем значения графика
-    allocate(x_func(points_count))
-    allocate(y_func(points_count))
-    do i = 1, points_count
-        x_func(i) = xmin + (xmax - xmin) * real(i - 1) / real(points_count - 1)
-        y_func(i) = coeffs(1) + coeffs(2) * x_func(i) + coeffs(3) * x_func(i)**2 + coeffs(4) * x_func(i)**3
-    end do
-
-
-    ! открытие окна и отрисовка
-    call plinit()
-    call plenv(xmin, xmax, ymin, ymax, 0, 0)
-    call pllab('t', 'C', '')
-    call plline(x_func, y_func)
-    call plpoin(x, y, 9)
-
-
-    ! закрываем программу
-    call plend()
-    deallocate(x)
-    deallocate(y)
-    deallocate(x_func)
-    deallocate(y_func)
-
-end program main
+end program slau_solver
