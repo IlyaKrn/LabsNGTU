f2a32c45d868983e006f99c6defba5c9be86d3d2
IlyaKrn <ilyakornoukhov@gmail.com>
2026-02-20 12:18:35 +0300

    c++

--- main.cpp
+++ main.cpp
@@ -7,7 +7,41 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-    solveSNAU({-1, 1});
+    //вычисляем точки
+    vector<long double> start1 = {1, -1};
+    vector<long double> start2 = {-1, 1};
+    vector<long double> appr1 = solveSNAU(start1);
+    vector<long double> appr2 = solveSNAU(start2);
+
+    //размеры окна и размер клетки
+    int HEIGHT = 1000;
+    int WIDTH = 1000;
+    int SCALE = 50;
+
+
+    //открываем окно и получаем матрицу пикселей
+    SDL_Window* window = SDL_CreateWindow("СНАУ", 0, 0, HEIGHT, WIDTH , 0);
+    Uint32* pixels = (Uint32*) SDL_GetWindowSurface(window)->pixels;
+
+
+    //отрисовываем функции
+    while (true) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if(event.type == SDL_QUIT) {
+                SDL_Quit();
+                return 0;
+            }
+        }
+
+        //отрисовка функций
+        for (int i = 0; i < HEIGHT; ++i) {
+            for (int j = 0; j < WIDTH; ++j) {
+                pixels[i * WIDTH + j] = 125454;
+            }
+        }
+        SDL_UpdateWindowSurface(window);
+    }
 
 
 }
