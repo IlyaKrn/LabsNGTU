7ed08b518198f05d7c320bde4d4952034a15e1fc
IlyaKrn <ilyakornoukhov@gmail.com>
2026-02-20 22:23:52 +0300

    c++

--- main.cpp
+++ main.cpp
@@ -5,12 +5,52 @@
 
 using namespace std;
 
+const int COLOR_WHITE = 2147483647;
+const int COLOR_GRAY = 1002159035;
+const int COLOR_RED = 16711680;
+const int COLOR_BLACK = 0;
+
+//рисуем клетки и оси
+void drawField(Uint32* pixels, int h, int w, int s){
+    for (int i = 0; i < h; ++i) {
+        for (int j = 0; j < w; ++j) {
+            pixels[i * w + j] = COLOR_WHITE;
+        }
+    }
+
+    for (int i = 0; i < h / 2 - 1; ++i) {
+        if(i % s == 0) {
+            for (int j = 0; j < w; ++j) {
+                pixels[(i + h / 2) * w + j] = COLOR_GRAY;
+                pixels[(-i + h / 2) * w + j] = COLOR_GRAY;
+            }
+        }
+    }
+    for (int i = 0; i < w / 2 - 1; ++i) {
+        if(i % s == 0) {
+            for (int j = 0; j < h; ++j) {
+                pixels[j * w + ((i + w / 2 - 1))] = COLOR_GRAY;
+                pixels[j * w + ((-i + w / 2 - 1))] = COLOR_GRAY;
+            }
+        }
+    }
+    for (int i = 0; i < w; ++i) {
+        pixels[(h / 2) * w + i] = COLOR_BLACK;
+    }
+    for (int i = 0; i < h; ++i) {
+        pixels[i * w + (w / 2)] = COLOR_BLACK;
+    }
+
+    //рисуем буквы
+
+}
+
 void drawPoint(int x, int y, int mx, int my, Uint32* pixels){
     int r = 15;
     for (int i = -r; i < r; ++i) {
         for (int j = -r; j < r; ++j) {
             if(y+i >= 0 && y+i < my && x+j >= 0 && x+j < mx && sqrt(i*i + j*j) < r && (sqrt(i*i + j*j) > r*0.7 || sqrt(i*i + j*j) < r*0.5))
-                pixels[(y+i) * mx + x+j] = 0;
+                pixels[(y+i) * mx + x+j] = COLOR_RED;
         }
     }
 }
@@ -33,7 +73,8 @@ int main(int argc, char** argv) {
     SDL_Window* window = SDL_CreateWindow("СНАУ", 0, 0, HEIGHT, WIDTH , 0);
     Uint32* pixels = (Uint32*) SDL_GetWindowSurface(window)->pixels;
 
-
+    //рисуем поле
+    drawField(pixels, HEIGHT, WIDTH, SCALE);
     //отрисовываем функции
     while (true) {
         SDL_Event event;
@@ -53,9 +94,7 @@ int main(int argc, char** argv) {
                 long double f2 = 1.5 * x * x - ((y * y) / 0.36) - 1;
 
                 if(abs(f1) < 0.004 || abs(f2) < 0.015)
-                    pixels[i * WIDTH + j] = 0;
-                else
-                    pixels[i * WIDTH + j] = 2147483647;
+                    pixels[i * WIDTH + j] = COLOR_BLACK;
             }
         }
 
