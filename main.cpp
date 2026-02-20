a5bb502c43b4820a5d821210b25744eb022c6e70
IlyaKrn <ilyakornoukhov@gmail.com>
2026-02-20 18:33:16 +0300

    c++

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@
 
 using namespace std;
 
+void drawPoint(int x, int y, int mx, int my, Uint32* pixels){
+    int r = 15;
+    for (int i = -r; i < r; ++i) {
+        for (int j = -r; j < r; ++j) {
+            if(y+i >= 0 && y+i < my && x+j >= 0 && x+j < mx && sqrt(i*i + j*j) < r && (sqrt(i*i + j*j) > r*0.7 || sqrt(i*i + j*j) < r*0.5))
+                pixels[(y+i) * mx + x+j] = 0;
+        }
+    }
+}
+
 int main(int argc, char** argv) {
 
     //вычисляем точки
@@ -16,7 +26,7 @@ int main(int argc, char** argv) {
     //размеры окна и размер клетки
     int HEIGHT = 1000;
     int WIDTH = 1000;
-    int SCALE = 50;
+    int SCALE = 250;
 
 
     //открываем окно и получаем матрицу пикселей
@@ -37,9 +47,23 @@ int main(int argc, char** argv) {
         //отрисовка функций
         for (int i = 0; i < HEIGHT; ++i) {
             for (int j = 0; j < WIDTH; ++j) {
-                pixels[i * WIDTH + j] = 125454;
+                long double x = (long double) (j - WIDTH / 2) / SCALE;
+                long double y = (long double) -(i - HEIGHT / 2) / SCALE;
+                long double f1 = cos(0.4 * y + x * x) + y * y + x * x - 1.6;
+                long double f2 = 1.5 * x * x - ((y * y) / 0.36) - 1;
+
+                if(abs(f1) < 0.004 || abs(f2) < 0.015)
+                    pixels[i * WIDTH + j] = 0;
+                else
+                    pixels[i * WIDTH + j] = 2147483647;
             }
         }
+
+        //отрисовка точек
+        drawPoint(start1[0] * SCALE + WIDTH / 2, -start1[1] * SCALE + HEIGHT / 2, WIDTH, HEIGHT, pixels);
+        drawPoint(start2[0] * SCALE + WIDTH / 2, -start2[1] * SCALE + HEIGHT / 2, WIDTH, HEIGHT, pixels);
+        drawPoint(appr1[0] * SCALE + WIDTH / 2, -appr1[1] * SCALE + HEIGHT / 2, WIDTH, HEIGHT, pixels);
+        drawPoint(appr2[0] * SCALE + WIDTH / 2, -appr2[1] * SCALE + HEIGHT / 2, WIDTH, HEIGHT, pixels);
         SDL_UpdateWindowSurface(window);
     }
 
