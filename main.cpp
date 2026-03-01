005a2afed61773e10b5f9bbca502465df9c84a61
IlyaKrn <ilyakornoukhov@gmail.com>
2026-03-01 21:59:42 +0300

    тз

--- main.cpp
+++ main.cpp
@@ -9,9 +9,11 @@ int main(int argc, char** argv) {
 
     int n = 3;
     int m = -3;
-    auto matrix = getInterpolationMatrix(n, 1, 0, 0, 0, [](long double x) -> long double {
-        return 0;
-    });
+    int maxIter = 7;
+    long double eps = 1e-3;
+
+    auto matrix = getInterpolationMatrix(n, 1, 0, 0, 0, [](long double x) -> long double {return 0;});
+
     cout << "Матрица задачи для n=" << n << endl;
     for (int i = 0; i < matrix.size(); ++i) {
         for (int j = 0; j < matrix.size(); ++j) {
@@ -20,7 +22,9 @@ int main(int argc, char** argv) {
         cout << endl;
     }
 
-    cout << "Собственное значение при степенном методе\n" << getLambdaStep(matrix, 1e-100, 100) << endl;
-    cout << "Собственное значение при методе обратных итераций и m=" << m << "\n" << getLambdaBackIter(matrix, m, 1e-100, 100) << endl;
+    cout << "Погрешность: " << eps << endl;
+    cout << "Максимальное число итераций: " << maxIter << endl;
+    cout << "Собственное значение при степенном методе:\n" << getLambdaStep(matrix, eps, maxIter) << endl;
+    cout << "Собственное значение при методе обратных итераций и m = " << m << ":\n" << getLambdaBackIter(matrix, m, eps, maxIter) << endl;
 
 }
