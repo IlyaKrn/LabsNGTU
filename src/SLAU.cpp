0dc553acb960c7c7f6777119ff3b7cac78dfecaf
IlyaKrn <ilyakornoukhov@gmail.com>
2026-03-31 02:03:55 +0300

    фикс?

--- src/SLAU.cpp
+++ src/SLAU.cpp
@@ -274,7 +274,7 @@ vector<vect_t> shikhman(vect_t start, long double maxTime, long double eps_i, lo
             vector<vector<long double>> res = rhsJ({cur, curYktNext.t});
             for (int i = 0; i < res.size(); ++i) {
                 for (int j = 0; j < res.size(); ++j) {
-                    res[i][j] = 1 - b0 * res[i][j];
+                    res[i][j] = (i == j ? 1 : 0) - b0 * res[i][j];
                 }
             }
             return res;
