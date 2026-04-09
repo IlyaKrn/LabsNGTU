e3f7967effafd072d14cb7fe7d3d9fc992b1a711
IlyaKrn <ilyakornoukhov@gmail.com>
2026-04-09 18:22:40 +0300

    7 лабораторная - bert

--- bot.py
+++ bot.py
@@ -2,11 +2,143 @@ import enum
 import requests
 import spacy
 import re
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import logger
 import pandas as pd
 import joblib
 import numpy as np
+import torch
+from transformers import AutoTokenizer, AutoModel
+from transformers import AutoModelForSequenceClassification
+from transformers import Trainer, TrainingArguments
+
+def train_bert_model():
+    tokenizer = AutoTokenizer.from_pretrained("DeepPavlov/rubert-base-cased")
+    model = AutoModel.from_pretrained("DeepPavlov/rubert-base-cased")
+
+    def bert_vector(text):
+        inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
+        outputs = model(**inputs)
+        cls_vector = outputs.last_hidden_state[:, 0, :]
+        return cls_vector.detach().numpy()[0]
+
+    data = pd.read_csv("dataset.csv")
+    texts = [preprocess(text) for text in data["text"]]
+    labels = data["intent"]
+
+    X = np.array([bert_vector(text) for text in texts])
+    y = labels
+
+    clf = LogisticRegression(max_iter=1000)
+    clf.fit(X, y)
+    return clf
+
+
+
+
+
+def train_bert_classifier():
+    MODEL_NAME = "DeepPavlov/rubert-base-cased"
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME,num_labels=3)
+
+    df = pd.read_csv("dataset.csv")
+
+    label2id = {label: idx for idx, label in enumerate(df["intent"].unique())}
+    id2label = {v: k for k, v in label2id.items()}
+
+    df["label"] = df["intent"].map(label2id)
+
+    train_texts, val_texts, train_labels, val_labels = train_test_split(
+        df["text"].tolist(), df["label"].tolist(), test_size=0.2)
+
+
+
+    def tokenize(texts):
+        return tokenizer(texts,
+            padding=True,
+            truncation=True,
+            return_tensors="pt"
+        )
+    train_encodings = tokenize(train_texts)
+    val_encodings = tokenize(val_texts)
+
+    class IntentDataset(torch.utils.data.Dataset):
+        def __init__(self, encodings, labels):
+            self.encodings = encodings
+            self.labels = labels
+
+        def __getitem__(self, idx):
+            item = {key: val[idx] for key, val in self.encodings.items()}
+            item["labels"] = torch.tensor(self.labels[idx])
+            return item
+
+        def __len__(self):
+            return len(self.labels)
+
+    train_dataset = IntentDataset(train_encodings, train_labels)
+    val_dataset = IntentDataset(val_encodings, val_labels)
+
+    training_args = TrainingArguments(
+        output_dir="./results",
+        num_train_epochs=4,
+        per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
+        eval_strategy="epoch",
+        logging_dir="./logs",
+        learning_rate=2e-5,
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+    )
+
+    trainer.train()
+
+    def predict_intent(text):
+        inputs = tokenizer(text, return_tensors="pt", truncation=True)
+        outputs = model(**inputs)
+        logits = outputs.logits
+        predicted_class = torch.argmax(logits, dim=1).item()
+        return id2label[predicted_class]
+
+    model.save_pretrained("intent_model")
+    tokenizer.save_pretrained("intent_model")
+
+    return predict_intent
+
+
+
+
+
+def load_bert_bot():
+    MODEL_PATH = "intent_model"
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
+    model.eval()
+
+    label_map = {
+        0: "GREETING",
+        1: "GOODBYE",
+        2: "WEATHER"
+    }
+
+    def predict_intent(text: str) -> str:
+        inputs = tokenizer(text, return_tensors="pt", truncation=True)
+        with torch.no_grad():
+            outputs = model(**inputs)
+        logits = outputs.logits
+        predicted_class = torch.argmax(logits, dim=1).item()
+        return label_map[predicted_class]
+
+    return predict_intent
+
+
+nlp = spacy.load("ru_core_news_md")
 
 # состояния автомата
 class States:
@@ -22,45 +154,10 @@ class States:
 intents = [
     'GREETING',
     'GOODBYE',
-    'ADDITION',
     'WEATHER',
+    'ADDITION',
 ]
 
-# анализатор строк
-nlp = spacy.load("ru_core_news_md")
-
-# нормализация
-def preprocess(text):
-    tokens = []
-    for token in nlp(text):
-        if not token.is_stop and not token.is_punct:
-            tokens.append(token.lemma_)
-    return " ".join(tokens)
-
-# нормализация
-def vectorize(text):
-    return nlp(text).vector
-
-# датасет
-data = pd.read_csv("dataset.csv")
-texts = [preprocess(text) for text in data.iloc[:, 0]]
-labels = data.iloc[:, 1]
-
-# векторизация
-X = np.array([vectorize(text) for text in texts])
-
-# модель
-model = LogisticRegression()
-model.fit(X, labels)
-
-# получение intent
-def intent(text):
-    vector = vectorize(text).reshape(1, -1)
-    proba = model.predict_proba(vector)
-    if max(proba[0]) < 0.3:
-        return None
-    print(model.predict(vector))
-    return model.predict(vector)[0]
 
 # выражения
 addition_regex = re.compile(r"(\A|\s)([\d\s*\+\s*]+\d)(\Z|\s)",re.IGNORECASE)
@@ -79,6 +176,8 @@ if __name__ == "__main__":
     lastStates = []
     state = States.START
     message = ''
+    # train_bert_classifier()
+    intent = load_bert_bot()
     while state != States.END:
         lastStates.append(state)
         match state:
